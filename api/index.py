8a1b4f32b8c67bc7782a0c6cb607b552c84cc369
txabiii <txabiguerrero2000@gmail.com>
2023-05-03 16:27:55 +0800

    Add test

--- api/index.py
+++ api/index.py
@@ -4,8 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+  return 'Hello, World!'
 
 @app.route('/about')
 def about():
-    return 'About'
+  return 'About'
+
+@app.route('/test')
+def test():
+  return 'Testing'
+
+if __name__ == '__main__':
+  app.run(port=8000)
